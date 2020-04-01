de7242043ca25d24304cf8809b101eaefb89138e
maxskr <maxskrin03@gmail.com>
2020-04-01 20:41:38 +0400

    Тест

---
+++ Appendix/First/Db/Db.js
@@ -0,0 +1,47 @@
+var Reposit = function (key, localStorage){
+    this.key = key;
+    this.localStorage = localStorage;
+}
+
+Reposit.prototype.Get = function (){
+    return this.localStorage.getItem(this.key);
+}
+
+Reposit.prototype.Set = function (content){
+    this.localStorage.setItem(this.key, content);
+}
+
+var TasksReposit = function (localStorage){
+    Reposit.call(this, 'tasks', localStorage);
+}
+
+TasksReposit.prototype = Object.create(Reposit.prototype);
+
+TasksReposit.prototype.Get = function(){
+    return JSON.parse(Reposit.prototype.Get.call(this));
+}
+
+TasksReposit.prototype.Add = function (elem){
+    var arr = this.Get();
+    arr.push(elem);
+    this.Set(arr);
+}
+
+TasksReposit.prototype.Remove = function (elemId){
+    var arr = this.Get();
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i].id == elemId) {
+            arr.splice(i, 1);
+            break;
+        }
+    }
+    this.Set(arr);
+}
+
+TasksReposit.prototype.Set = function(content){
+    Reposit.prototype.Set.call(this, JSON.stringify(content));
+}
+
+
+
+
---
+++ Appendix/First/main.js
@@ -0,0 +1,35 @@
+var task = new TasksReposit(window.localStorage);
+var btn = document.querySelector('#btn');
+var content = document.querySelector('#content').value;
+btn.onclick = function (){
+    var content = document.querySelector('#content').value;
+    var title = document.querySelector('#title').value;
+    task.Add(new Note(content, title));
+    console.log(task.Get());
+    renderTable(task)
+}
+
+renderTable(task)
+function renderTable(task){
+    var table = document.querySelector('#table');
+    table.innerHTML = '';
+    var tr = document.createElement('tr');
+    var td1 = document.createElement('td');
+    td1.innerHTML = 'Заголовок'; 
+    var td2 = document.createElement('td');
+    td2.innerHTML = 'Текст'; 
+    table.append(tr);
+    tr.append(td1);
+    tr.append(td2);
+    for(var i = 0; i < task.Get().length; i++){
+        var tr = document.createElement('tr');
+        table.append(tr);
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        td1.innerHTML = task.Get()[i].title;
+        td2.innerHTML = task.Get()[i].content;
+        tr.append(td1);
+        tr.append(td2);
+    }
+}
+
